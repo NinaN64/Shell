c807e17c5d423e77878280cd372d90ab211fb130
Nina <nowaknina648@gmail.com>
2026-01-06 11:32:07 +0100

    locating files

--- src/Program.cs
+++ src/Program.cs
@@ -2,18 +2,49 @@ class Program
 {
     static void Main()
     {
-        // TODO: Uncomment the code below to pass the first stage
+        List<string> pathVariable =
+        Environment.GetEnvironmentVariable("PATH").Split(':').ToList();
+        var TypeOfCommands = new HashSet<string> { "type", "exit", "echo" };
+
         while(true)
         {
             Console.Write("$ ");
-            var TypeOfCommands = new HashSet<string> { "type", "exit", "echo" };
             string command = Console.ReadLine();
+
             if(command.ToLower() == "exit") break;
             else if(command.StartsWith("type"))
-                if(TypeOfCommands.Any(x => TypeOfCommands.Contains(command.Substring(5))))
-                    Console.WriteLine(command.Substring(5) + " is a shell builtin");
+            {
+                var argument = command.Substring(5);
+
+                if(TypeOfCommands.Any(x => TypeOfCommands.Contains(argument)))
+                {
+                    Console.WriteLine(argument + " is a shell builtin");
+                }
                 else
-                    Console.WriteLine(command.Substring(5) + " not found");
+                {
+                    string firstOrDefault = null;
+                    foreach (var path in pathVariable)
+                    {
+                        string fullPath = path + "/" + argument;
+
+                        if (!File.Exists(fullPath))
+                            continue;
+
+                        var mode = File.GetUnixFileMode(fullPath);
+                        if ((mode & UnixFileMode.UserExecute) != 0)
+                        {
+                            firstOrDefault = fullPath;
+                            break;
+                        }
+                    }
+
+                if (!string.IsNullOrWhiteSpace(firstOrDefault)) {
+                    Console.WriteLine($"{argument} is {firstOrDefault}");
+                } else {
+                    Console.WriteLine($"{argument}: not found");
+                }
+                }
+            }
             else if(command.StartsWith("echo"))
                 Console.WriteLine(command.Substring(5));
             else
